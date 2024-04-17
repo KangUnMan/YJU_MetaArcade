f888c09089a00d82143038f06411060700e771ad
bson343 <byeonggukson@gmail.com>
2024-04-17 10:35:23 +0900

    로그인 로직 구현 아직 웹서버가 로컬에만 존재합니다. 제 컴퓨터로만 될 듯...
    DB: MS SQL Server
    ORM: EntityframeworkCore
    WebServer: .NET

---
+++ Assets/Private/bson/Script/UI_Login.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using WebSocketSharp;
+
+public class UI_Login : MonoBehaviour
+{
+    [SerializeField] TMP_Text infoText;
+    [SerializeField] TMP_InputField loginText;
+    [SerializeField] TMP_InputField passwordText;
+    [SerializeField] Button signUpBtn;
+    [SerializeField] Button signInBtn;
+
+    const string tempUrl = "https://localhost:7130/api";
+
+    private void Awake()
+    {
+        signUpBtn.onClick.AddListener(OnClickSignUp);
+        signInBtn.onClick.AddListener(OnClickSignIn);
+        infoText.text = "";
+    }
+
+    void OnClickSignUp()
+    {
+        if (string.IsNullOrEmpty(loginText.text) || string.IsNullOrEmpty(passwordText.text))
+        {
+            Debug.Log("usage error; need fill email or password");
+            return;
+        }
+
+        infoText.text = "";
+
+        CreateAccountPacketReq packet = new CreateAccountPacketReq()
+        {
+            AccountName = loginText.text,
+            Password = passwordText.text,
+        };
+        StartCoroutine(PostSend<CreateAccountPacketRes>("Account/create", packet, (res) =>
+        {
+            Debug.Log(res.CreateOk);
+            infoText.text = $"SignUP: {res.CreateOk}";
+            loginText.text = "";
+            passwordText.text = "";
+        }));
+    }
+
+    void OnClickSignIn()
+    {
+        if (string.IsNullOrEmpty(loginText.text) || string.IsNullOrEmpty(passwordText.text))
+        {
+            Debug.Log("usage error; need fill email or password");
+            return;
+        }
+
+        infoText.text = "Login...";
+
+        LoginAccountPacketReq packet = new LoginAccountPacketReq()
+        {
+            AccountName = loginText.text,
+            Password = passwordText.text,
+        };
+        StartCoroutine(PostSend<LoginAccountPacketRes>("Account/login", packet, (res) =>
+        {
+            Debug.Log(res.LoginOk);
+            infoText.text = $"LogIn: {res.LoginOk}";
+            loginText.text = "";
+            passwordText.text = "";
+
+            if (res.LoginOk)
+            {
+                SceneManager.LoadScene("Map");
+            }
+        }));
+    }
+
+    IEnumerator PostSend<T>(string url, object obj, Action<T> res)
+    {
+        string sendUrl = $"{tempUrl}/{url}";
+
+        byte[] jsonBytes = null;
+        if (obj != null)
+        {
+            string jsonStr = JsonUtility.ToJson(obj);
+            jsonBytes = Encoding.UTF8.GetBytes(jsonStr);
+        }
+
+        using (var uwr = new UnityWebRequest(sendUrl, "POST"))
+        {
+            uwr.uploadHandler = new UploadHandlerRaw(jsonBytes);
+            uwr.downloadHandler = new DownloadHandlerBuffer();
+            uwr.SetRequestHeader("Content-Type", "application/json");
+
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+                infoText.text = uwr.error;
+            }
+            else
+            {
+                //Debug.Log(uwr.downloadHandler.text);
+                T resObj = JsonUtility.FromJson<T>(uwr.downloadHandler.text);
+                res.Invoke(resObj);
+            }
+        }
+    }
+}
---
+++ Assets/Private/bson/Script/WebPacket.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+public class CreateAccountPacketReq
+{
+    public string AccountName;
+    public string Password;
+}
+
+public class CreateAccountPacketRes
+{
+    public bool CreateOk;
+}
+
+public class LoginAccountPacketReq
+{
+    public string AccountName;
+    public string Password;
+}
+
+public class ServerInfo
+{
+    public string Name;
+    public string Ip;
+    public int CrowdedLevel;
+}
+
+public class LoginAccountPacketRes
+{
+    public bool LoginOk;
+}
