6695f9fb85599edf42706e402d26c5b8aa54f8b7
bson343 <byeonggukson@gmail.com>
2024-06-19 22:26:13 +0900

    웹서버 통신용 패킷

--- Assets/Private/bson/Script/WebPacket.cs
+++ Assets/Private/bson/Script/WebPacket.cs
@@ -4,31 +4,60 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections;
 
+[System.Serializable]
 public class CreateAccountPacketReq
 {
-    public string AccountName;
-    public string Password;
+    public string AccountName { get; set; }
+    public string Password { get; set; }
 }
 
+[System.Serializable]
 public class CreateAccountPacketRes
 {
-    public bool CreateOk;
+    
+    public bool CreateOk { get; set; }
 }
 
+[System.Serializable]
 public class LoginAccountPacketReq
 {
-    public string AccountName;
-    public string Password;
+    public string AccountName { get; set; }
+    public string Password { get; set; }
 }
 
 public class ServerInfo
 {
-    public string Name;
-    public string Ip;
-    public int CrowdedLevel;
+    public string Name { get; set; }
+    public string Ip { get; set; }
+    public int CrowdedLevel { get; set; }
 }
 
+[System.Serializable]
 public class LoginAccountPacketRes
 {
-    public bool LoginOk;
+    public bool LoginOk { get; set; }
+    public string Token { get; set; }
+    public string UserName { get; set; }
+    public DateTime ExpiredTime { get; set; }
+}
+
+[System.Serializable]
+public class SetRankingPacketReq
+{
+    public string AccountName { get; set; }
+    public int Score { get; set; }
+}
+
+[System.Serializable]
+public class SetRankingPacketRes
+{
+    public bool SetRankOk { get; set; }
+}
+
+[System.Serializable]
+public class RankingPacketRes
+{
+    public string AccountName { get; set; }
+    public int Score { get; set; }
+    public DateTime Date { get; set; }
 }
